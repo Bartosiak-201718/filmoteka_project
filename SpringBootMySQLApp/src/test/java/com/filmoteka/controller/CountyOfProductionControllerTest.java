a7596233a93732ead2f234df088cbbc504a31792
Michal Siedlecki <203989@edu.p.lodz.pl>
2018-06-11 01:36:24 +0200

    Dodano metody post, put i delete dla Kraju

--- SpringBootMySQLApp/src/test/java/com/filmoteka/controller/CountyOfProductionControllerTest.java
+++ SpringBootMySQLApp/src/test/java/com/filmoteka/controller/CountyOfProductionControllerTest.java
@@ -2,9 +2,20 @@ package com.filmoteka.controller;
 
 import org.junit.Test;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
+import com.filmoteka.sdo.CountryOfProduction;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static io.restassured.RestAssured. *;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class CountyOfProductionControllerTest {
 
@@ -17,14 +28,42 @@ public class CountyOfProductionControllerTest {
 
     @Test
     public void deleteCountryOfProduction() {
+        given()
+                .when()
+                .delete("/api/country/3")
+                .then()
+                .statusCode(200);
     }
 
     @Test
     public void createCountryOfProduction() {
+        CountryOfProduction country = new CountryOfProduction();
+        country.setName ("Belgium");
+
+        given().body (country)
+                .when ()
+                .contentType (ContentType.JSON)
+                .post ("/api/country");
+
+        get("/api/country/4").then().statusCode(200).assertThat()
+                .body("name", equalTo("Belgium"))
+                .body("id", equalTo(4));
     }
 
     @Test
     public void updateCountryOfProduction() {
+        CountryOfProduction country = new CountryOfProduction();
+        country.setId((long) 2);
+        country.setName ("Ukraine");
+
+        given().body (country)
+                .when ()
+                .contentType (ContentType.JSON)
+                .put ("/api/country/2");
+
+        get("/api/country/2").then().statusCode(200).assertThat()
+                .body("name", equalTo("Ukraine"))
+                .body("id", equalTo(2));
     }
 
     @Test
