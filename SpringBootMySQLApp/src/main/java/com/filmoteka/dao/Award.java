1fb155cf4400ad2aa569e46432b18abb2779a3c1
Michal Siedlecki <203989@edu.p.lodz.pl>
2018-06-11 01:27:31 +0200

    Dodano metody post, put i delete dla nagrody

--- SpringBootMySQLApp/src/main/java/com/filmoteka/dao/Award.java
+++ SpringBootMySQLApp/src/main/java/com/filmoteka/dao/Award.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class Award implements Serializable{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
--- SpringBootMySQLApp/src/test/java/com/filmoteka/controller/AwardControllerTest.java
+++ SpringBootMySQLApp/src/test/java/com/filmoteka/controller/AwardControllerTest.java
@@ -2,29 +2,68 @@ package com.filmoteka.controller;
 
 import org.junit.Test;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.equalTo;
+import com.filmoteka.sdo.Award;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import static io.restassured.RestAssured. *;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class AwardControllerTest {
 
     @Test
     public void getAward() {
-        get("/api/awards/100").then().statusCode(200).assertThat()
+        get("/api/awards/1").then().statusCode(200).assertThat()
                 .body("name", equalTo("BAFTA"))
-                .body("id", equalTo(100));
+                .body("id", equalTo(1));
     }
 
     @Test
     public void deleteAward() {
+        given().
+                when()
+                .delete("/api/awards/3")
+                .then()
+                .statusCode(200);
     }
 
     @Test
-    public void createAward() {
+    public void createAward() throws JSONException {
+        Award award = new Award();
+        award.setName("MTV Music Award 2016");
+
+        given().body (award)
+                .when()
+                .contentType(ContentType.JSON)
+                .post("/api/awards");
+
+        get("/api/awards/4").then().statusCode(200).assertThat()
+                .body("name",equalTo("MTV Music Award 2016"))
+                .body("id",equalTo(4));
     }
 
     @Test
     public void updateAward() {
+        Award award = new Award();
+        award.setId((long) 2);
+        award.setName ("Gotham Awards");
+
+        given().body (award)
+                .when ()
+                .contentType (ContentType.JSON)
+                .put ("/api/awards/2");
+
+        get("/api/awards/2").then().statusCode(200).assertThat()
+                .body("name", equalTo("Gotham Awards"))
+                .body("id", equalTo(2));
     }
 
     @Test
