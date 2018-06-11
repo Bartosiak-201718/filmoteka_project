7fffdfcc3b107f79055d3a4b79bb52289c120f45
Bartek <gradek96@o2.pl>
2018-06-12 00:28:12 +0200

    Testy dla distributor

--- SpringBootMySQLApp/src/test/java/com/filmoteka/controller/DistributorControllerTest.java
+++ SpringBootMySQLApp/src/test/java/com/filmoteka/controller/DistributorControllerTest.java
@@ -1,9 +1,13 @@
 package com.filmoteka.controller;
 
+import com.filmoteka.sdo.Distributor;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.*;
 
 public class DistributorControllerTest {
@@ -17,17 +21,52 @@ public class DistributorControllerTest {
 
     @Test
     public void deleteDistributor() {
+        given()
+                .when()
+                .delete("/api/distributors/13")
+                .then()
+                .statusCode(200);
     }
 
     @Test
     public void createDistributor() {
+        Distributor distributor = new Distributor();
+        distributor.setName("Warner Bros");
+
+        given().body(distributor)
+                .when()
+                .contentType(ContentType.JSON)
+                .post("/api/distributors");
+
+        get("/api/distributors/1").then().statusCode(200).assertThat()
+                .body("name", equalTo("Warner Bros"))
+                .body("id", equalTo(1));
     }
 
     @Test
     public void updateDistributor() {
+        Distributor distributor = new Distributor();
+        distributor.setId((long) 12);
+        distributor.setName("MCU");
+
+        given().body(distributor)
+                .when()
+                .contentType(ContentType.JSON)
+                .put("/api/distributors/12");
+
+        get("/api/distributors/12").then().statusCode(200).assertThat()
+                .body("name", equalTo("MCU"))
+                .body("id", equalTo(12));
     }
 
     @Test
     public void getAllDistributor() {
+        given()
+                .when()
+                .get("/api/distributors")
+                .then()
+                .assertThat()
+                .body("id", hasItems(1, 11, 12))
+                .body("name", hasItems("Polska Film", "MCU", "Warner Bros"));
     }
 }
